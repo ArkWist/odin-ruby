446e0d5f1a2ab8b2fca59cb355ece5a3efb0d31c
Daniel A. G. Lilley <arkwist@gmail.com>
2016-02-25 16:57:38 +0900

    Write user input capture

---
+++ cipher.rb
@@ -0,0 +1,11 @@
+def caesar_cipher(original_string, shift_factor)
+
+end
+
+puts "Enter input string: "
+original_string = gets.chomp
+puts "Enter shift factor: "
+shift_factor = gets.chomp
+
+ciphered_string = caesar_cipher(original_string, shift_factor)
+puts "Result: #{ciphered_string}"
