d56b5466f7a380469fd89d38307d9e669eb8cff2
Daniel A. G. Lilley <arkwist@gmail.com>
2016-02-26 22:28:42 +0900

    Make it work if shift factor doesn't exceed top letter

--- cipher.rb
+++ cipher.rb
@@ -1,27 +1,33 @@
-# a-z roll
-# A-Z roll
-# Leave all else the same
-
 def caesar_cipher(string, shift_factor)
-  string.map do |letter|
+
+  new_string = string.each_char.map do |letter|
     letter = shift_if_range(letter, shift_factor, "a".ord, "z".ord)
     letter = shift_if_range(letter, shift_factor, "A".ord, "Z".ord)
   end
-  string
+
+  new_string
 end
 
 def shift_if_range(letter, shift_factor, range_start, range_end)
+
   letter_number = letter.ord
-  if letter_number >= range_start && letter_number <= range_end do
+  
+  if letter_number >= range_start && letter_number <= range_end then
+
     # Sanitize shift_factor so don't need while loop
-    shift_factor = (range_end - range_start) % shift_factor
+    shift_factor = shift_factor.to_i
+    shift_factor = shift_factor % (range_end - range_start)
+    
     letter_number += shift_factor
-    if letter_number > range_end do
+
+    if letter_number > range_end then
       shift_factor = range_end - letter_number
       letter_number = range_start + shift_factor
     end
+
     letter = letter_number.chr
   end
+
   letter
 end
 
@@ -30,5 +36,5 @@ original_string = gets.chomp
 puts "Enter shift factor: "
 shift_factor = gets.chomp
 
-ciphered_string = caesar_cipher(original_string, shift_factor)
-puts "Result: #{ciphered_string}"
+ciphered_string = caesar_cipher(original_string, shift_factor).join
+puts "Result: " + ciphered_string
