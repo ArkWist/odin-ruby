17e954b12d822d4502796fbf90f9f836d5657064
Daniel A. G. Lilley <arkwist@gmail.com>
2016-02-26 22:03:39 +0900

    Work in progress

--- cipher.rb
+++ cipher.rb
@@ -2,12 +2,27 @@
 # A-Z roll
 # Leave all else the same
 
-def caesar_cipher(original_string, shift_factor)
-  original_string.map do |char|
-    char = shift_if_range(char, shift_factor, 65, 90)
-    char = shift_if_range(char, shift_factor, 97, 122)
+def caesar_cipher(string, shift_factor)
+  string.map do |letter|
+    letter = shift_if_range(letter, shift_factor, "a".ord, "z".ord)
+    letter = shift_if_range(letter, shift_factor, "A".ord, "Z".ord)
   end
-  return original_string
+  string
+end
+
+def shift_if_range(letter, shift_factor, range_start, range_end)
+  letter_number = letter.ord
+  if letter_number >= range_start && letter_number <= range_end do
+    # Sanitize shift_factor so don't need while loop
+    shift_factor = (range_end - range_start) % shift_factor
+    letter_number += shift_factor
+    if letter_number > range_end do
+      shift_factor = range_end - letter_number
+      letter_number = range_start + shift_factor
+    end
+    letter = letter_number.chr
+  end
+  letter
 end
 
 puts "Enter input string: "
