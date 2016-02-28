5da93c297d09263f37f4f880d0ac3dc997708d8d
Daniel A. G. Lilley <arkwist@gmail.com>
2016-02-28 22:29:30 +0900

    Complete bubble sort for numbers

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,30 @@
-def bubble_sort (unsorted_array)
+def bubble_sort (array)
   
   sorted = false
 
   while !sorted
     sorted = true
-    unsorted_array[0...-1].map.with_index do |value, index|
 
-      if value < unsorted_array[index + 1]
-        temp = unsorted_array[index + 1]
-        unsorted_array[index + 1] = value
-        value = temp
+    index = 0
+    while index < array.length - 1
+    
+      if array[index] > array[index+1]
+        temp = array[index]
+        array[index] = array [index+1]
+        array[index+1] = temp
         sorted = false
       end
-
+      
+      index += 1
+      
     end
   end
+  return array
 end
 
 # print "Enter array to sort: "
 # unsorted_array = gets.chomp
 
 unsorted_array = [1,3,2,5,6,4]
-puts unsorted_array
-puts bubble_sort(unsorted_array)
+print bubble_sort(unsorted_array).inspect
+
