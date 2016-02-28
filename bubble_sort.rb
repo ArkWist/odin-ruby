4370ee427b7b9a1b0ece76caa03cd718f7d35d35
Daniel A. G. Lilley <arkwist@gmail.com>
2016-02-28 22:02:48 +0900

    Write first draft before testing.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,23 @@
-def bubble_sort
+def bubble_sort (unsorted_array)
+  
+  sorted = false
 
+  while !sorted
+    sorted = true
+    unsorted_array[0...-1].map.with_index do |value, index|
+
+      if value < unsorted_array[index + 1]
+        temp = unsorted_array[index + 1]
+        unsorted_array[index + 1] = value
+        value = temp
+        sorted = false
+      end
+
+    end
+  end
 end
+
+print "Enter array to sort: "
+unsorted_array = gets.chomp
+
+puts bubble_sort(unsorted_array)
