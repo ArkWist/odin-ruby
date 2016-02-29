254615699b04deb5ad0399f0710dad2aeebfb1c3
Daniel A. G. Lilley <arkwist@gmail.com>
2016-02-29 21:16:50 +0900

    Initial commit

---
+++ enumerable.rb
@@ -0,0 +1,19 @@
+module Enumerable
+
+  def my_each
+    # code!
+  end
+
+  #my_each_with_index
+  #my_select
+  #my_all?
+  #my_any?
+  #my_none?
+  #my_count
+  #my_map
+  #my_inject
+  #multiply_els
+  
+end
+
+
