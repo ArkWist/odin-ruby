591d567d3d218effc368597469ac3aa68be4b7ea
ArkWist <arkwist@gmail.com>
2016-03-01 14:12:04 +0900

    Draft my_each and my_each_with_index for later home testing

--- enumerable.rb
+++ enumerable.rb
@@ -1,19 +1,39 @@
 module Enumerable
 
-  def my_each
-    # code!
+  def my_each   # ; do the given block for everything in the array 
+    for element in self
+      yield(element)
+    end
   end
+  
+  def my_each_with_index
+    index = 0
+    for element in self
+      yield(element,index)
+      index += index
+    end
+  end
+  
+=begin # Alternative from Jberczel for reference if mine doesn't work
+  def my_each_with_index
+    for i in 0...length
+      yield(self[i], i)
+    end
+  end
+=end
 
-  #my_each_with_index
+  def my_select
+
   #my_select
-  #my_all?
-  #my_any?
+  #my_all?      ; does everything satisfy the block condition?
+  #my_any?      ; does anything satisfy the block condition?
   #my_none?
   #my_count
   #my_map
   #my_inject
-  #multiply_els
   
 end
 
+  #multiply_els
 
+  
