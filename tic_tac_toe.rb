fc6b67d404cc1fae428c760b1f49bca0327cc349
Daniel A. G. Lilley <arkwist@gmail.com>
2016-03-05 22:03:11 +0900

    Start planning main classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,24 @@
 # Tic-Tac-Toe
 
-def tic_tac_toe; end
+class TicTacToe
+  def initialize(row_count=3, column_count=3)
+    @player_1 = "X"
+    @player_2 = "O"
+    @rows = Array.new(row_count, Row.new(column_count))
+  end
+end
+
+class Row
+  def initialize(column_count=3)
+    @columns = Array.new(column_count, Cell.new)
+  end
+end
+
+class Cell
+  def initialize
+    @value = nil
+  end
+end
+    
+
+#def tic_tac_toe; end
