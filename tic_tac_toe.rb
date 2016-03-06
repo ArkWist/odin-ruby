8c82ea8beccf1251b4c15e2e88e2d52c6a185d17
Daniel A. G. Lilley <arkwist@gmail.com>
2016-03-06 21:12:52 +0900

    Write calls to main methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,33 +1,94 @@
 # Tic-Tac-Toe
 
 class TicTacToe
-  def initialize(row_count = 3, column_count = 3)
-    @board = TicTacToeBoard.new(row_count, column_count)
-    @player_1 = :X
-    @player_2 = :Y
+  def initialize
+    @board = set_board
+    @players = [Player.new('X'), Player.new('O')]
+    set_up_game
   end
-end
+  
+  private
+  
+  def set_up_game
+    #loop
+    #print "Default board? (y/n): "
+    #answer = gets.chomp.downcase
+    #answer_valid = valid_yes_no?(answer)
+    #if answer.downcase = "n"
+    #  @board = reset_board
+    #elsif
+    #puts "Rows?: "
+    #rows = gets.chomp
+    #puts "Columns?: "
+    #columns = gets.chomp
+    #puts "Player 1 is human?: "
+    #puts "Player 2 is human?: "
+    play
+  
+  def play
+    #set_board
+    #@board = TicTacToeBoard.new(@row_count, @column_count)
+    @board.draw_board
+    
+    #take turn, then check, then set next turn
+    game_over = false
+    current_player = 0
+    
+    while !game_over
+      @players[current_player].make_move
+      @board.draw_board
+      @board.check_victory(@players[current_player])
+      game_over = @board.game_over?
+      unless game_over { current_player = next_player(current_player) }
+    end
+    @board.end_game
+  end  
+  
+  #private
+  
+  def set_board(row_count = 3, column_count = 3)
+    TicTacToeBoard.new(row_count, column_count)
+  end
+
 
 # Not sure how best to handle players?
 # Perhaps as object in the board_
 # Or perhaps outside the board, in a game class?
 
-class TicTacToeBoard
-  def initialize(row_count = 3, column_count = 3)
-    @cells = Array.new(row_count) { Array.new(column_count) { Cell.new(:empty)} }
+  class TicTacToeBoard
+    def initialize(row_count = 3, column_count = 3)
+      @cells = Array.new(row_count) { Array.new(column_count) { Cell.new(:empty)} }
+    end
+
+    class Cell
+      attr_reader :value
+      def initialize(value = :empty)
+        @value = value
+      end
+    end
+
+  class Player
+    def initialize(icon = '#', human = true)
+      @icon = icon
+      @human = human
+    end
+    def is_human?
+      @human
+    end
+    def is_computer?
+      !@human
+    end
+    def set_icon(icon)
+      @icon = icon
+    end
+    def toggle_human_computer
+      @human = !@human
+    end
+      
+  
   end
-end
 
-
-class Cell
-  attr_reader :value
-  def initialize(value = :empty)
-    @value = value
-  end
-end
-
-
-loop do
+#loop do
   #puts "Play?: "
   game = TicTacToe.new
   #puts "Default board?: "
@@ -35,10 +96,10 @@ loop do
   #rows = gets.chomp
   #puts "Columns?: "
   #columns = gets.chomp
-  game.set_board_size(3, 3)
-  puts "Player 1 is human?: "
-  
-  puts "Player 2 is human?: "
+#  game.play
+  #puts "Player 1 is human?: "
+  #puts "Player 2 is human?: "
+  end
 end
 
 # Cell is an object that can hold player_1, player_2, or empty
