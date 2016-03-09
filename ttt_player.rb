4b71e3a218102b4ba87a3ecdd0c30753df5c6806
ArkWist <arkwist@gmail.com>
2016-03-09 14:07:11 +0900

    Include credible move checking

--- ttt_player.rb
+++ ttt_player.rb
@@ -1,49 +1,52 @@
-# Required by class::TicTacToe.
-# Shares namespace with class:TicTacToe.
+# Required by class::TTTGame.
+# Shares namespace with class::TTTGame.
 
 class Player
-  def initialize(icon = '?', controller = :unknown)
-    @icon = icon
-    @controller = controller
+  attr_reader :player_number, :human_controller, :player_icon
+
+  def initialize(icon = '?', human_controller = true)
+    @player_icon = icon
+    @human_controller = human_controller
   end
+  
   def is_human?
-    if @controller == :human
-      return true
-    else
-      return false
-    end
+    @human_controller
   end
+  
   def is_computer?
-    !@human
+    !@human_controller
   end
+  
   def set_icon(icon)
-    @icon = icon
+    @player_icon = icon
   end
   
-   
   def set_controller(controller)
-    case controller
-    when :human
-      @human = true
-    when :computer
-      @human = false
-    else
-      puts "Invalid controller set to human."
-      @human = true
-    end
+    @human_controller = controller
   end
-  
-  def get_controller
-    
     
   def toggle_human_computer
     @human = !@human
   end
   
   def make_move
-    print "Choose your position: "
-    answer = gets.chomp.downcase
-    if answer.is_empty?
+    credible_move = nil
+    until credible_move
+      print "Choose your position (ex: A2): "
+      move = gets.chomp.upcase
+      begin
+        column = move[0].order - 65
+        row = move[1].to_i
+      rescue
+        puts "Invalid position. Please choose a letter number combination."
+      else
+        row = move[0].order - 65
+        column = move[1].to_i
+        credible_move = Array.new(row, column)
+      end
     end
+    credible_move
   end
 end
+
+
