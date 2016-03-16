82063f88ade9fe5865c3617724e33452e6cf4d95
ArkWist <arkwist@gmail.com>
2016-03-16 12:23:09 +0900

    Remove minor redundancy

--- arg.rb
+++ arg.rb
@@ -5,7 +5,7 @@ class Arg
 
   def ==(other_arg)
     #same = if @arg == other_arg then true else false end
-    same = @arg == other_arg ? true : false
+    @arg == other_arg ? true : false
   end
 
 end
