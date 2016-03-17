e89e7c0320d994d1e717a7907d2dd26679392bb5
ArkWist <arkwist@gmail.com>
2016-03-17 13:19:40 +0900

    Create Pegs, Code, & Score classes

--- row.rb
+++ row.rb
@@ -1,1 +1,26 @@
-# uncommitted
+class Row
+
+  def initialize(length)
+    @code = Code.new(length)
+    @score = Score.new(length)
+  end
+
+end
+
+class Pegs
+
+  def initialize(length)
+    @pegs = Array.new(length)
+  end
+  
+end
+
+
+
+class Code < Pegs
+
+end
+
+class Score < Pegs
+
+end
