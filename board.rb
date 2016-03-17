be4ea2b059896445d1b4abe8bf8bdccbfccbd436
ArkWist <arkwist@gmail.com>
2016-03-17 21:02:37 +0900

    Update manually because Github is annoying

--- board.rb
+++ board.rb
@@ -1,10 +1,28 @@
 class Board
 
-  def initialize(rows, colors)
+  attr_reader :@guesses
+
+  def initialize(rows, colors, code_length)
     # @rows = [] << Row.new * rows
     # @rows[0...rows] = Row.new
-    @rows = Array.new(rows) { Row.new }
+    @answer = Row.new(length)
+    @guesses = Array.new(rows) { Row.new(length) }
     @colors = colors
+    @game_over = false
+    @victory = false
+  end
+  
+  def game_over(with_victory = false)
+    @game_over = true
+    @victory = with_victory
+  end
+  
+  def game_over?
+    @game_over
+  end
+  
+  def with_victory?
+    @victory
   end
   
 end
