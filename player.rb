f5f81948775f20e08e6102fa7bcf46fb04699bb2
ArkWist <arkwist@gmail.com>
2016-03-17 21:02:56 +0900

    Update manually because Github is annoying

--- player.rb
+++ player.rb
@@ -1,12 +1,64 @@
 class Player
 
+  def initialize
+  end
+
+  def victory
+    puts "#{@title} is victorious."
+  end
+
 end
 
 
 class Human < Player
 
+  def intialize(is_maker)
+    if is_maker
+      @title = "CODEMAKER"
+    else
+      @title = "CODEBREAKER"
+    end
+    @is_maker = is_maker
+  end
+
+  def get_code
+    print "#{@title} > Enter your code: "
+    gets.chomp
+  end
+  
+  def get_score
+    print "#{@title} > Enter your score (!/:/,): "
+    gets.chomp
+  end
+
 end
 
 class Comp < Player
+
+  def initialize(is_maker, colors, code_length)
+    if is_maker
+      @title = "CODEMAKER"
+    else
+      @title = "CODEBREAKER"
+    end
+    @is_maker = is_maker
+    @colors = colors
+    @code_length = code_length
+  end
+
+  def get_answer
+    if is_maker
+      answer += code_length.times { colors[Random.rand(code_length - 1)] }
+      puts "#{@title} has made a code."
+    else
+      puts "#{@title} has made a guess."
+    end
+    answer
+  end
+  
+  def get_score
+    score = ["."]
+  end
+
 
 end
