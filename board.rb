eebec935dcb2091285a43d891855fe8d475ee764
ArkWist <arkwist@gmail.com>
2016-03-17 11:23:04 +0900

    Initialize with guessing rows

--- board.rb
+++ board.rb
@@ -3,6 +3,7 @@ class Board
   def initialize(rows, colors)
     # @rows = [] << Row.new * rows
     # @rows[0...rows] = Row.new
+    @rows = Array.new(rows) { Row.new }
     @colors = colors
   end
   
