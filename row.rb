6b05c3e97114a646f723150759ffe5949796631f
ArkWist <arkwist@gmail.com>
2016-03-17 21:03:18 +0900

    Update manually because Github is annoying

--- row.rb
+++ row.rb
@@ -4,7 +4,15 @@ class Row
     @code = Code.new(length)
     @score = Score.new(length)
   end
-
+  
+  def print_row
+    puts ""
+  end
+  
+  def add_guess(guess)
+    @code.add(guess)
+  end
+  
 end
 
 class Pegs
@@ -13,14 +21,14 @@ class Pegs
     @pegs = Array.new(length)
   end
   
+  def add(pegs)
+    @pegs = pegs
+  end
+  
 end
 
-
-
 class Code < Pegs
-
 end
 
 class Score < Pegs
-
 end
