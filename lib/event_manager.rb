0b6e76e24ec75adc747a82eabf2e069bcc1ca342
ArkWist <arkwist@gmail.com>
2016-03-30 09:31:19 +0900

    Find legislators for zipcodes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,26 +1,30 @@
 ### Refactoring Clean Zip Codes
 
 require "csv"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "40cbccbbcc0f4ad0a8ce822a997a71e5"
 
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    zipcode = "00000"
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust 5, "0"
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, "0")[0..4]
 end
 
 puts "EventManager initialized."
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = Sunglight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislator_string = legislator_names.join(", ")
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
