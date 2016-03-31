787f7bd7ef73558660151e1703c093e7f452ddcc
ArkWist <arkwist@gmail.com>
2016-03-31 11:57:19 +0900

    Work on retrieving phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -22,12 +22,27 @@ def save_thank_you_letters(id, form_letter)
   end
 end
 
+def clean_number(number)
+  number = number.to_s.delete(/\D/).join(//)
+  if number.length == 11
+    number = number.delete(/^1/)
+  end
+  if number.length != 10
+    number = 0
+  else
+    number = number.to_i
+  end
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 template_letter = File.read "form_letter.erb"
-erb_template = ERB.new template_letter
+letter_template = ERB.new template_letter
+
+template_numbers = File.read "form_numbers.erb"
+number_template = ERB.new template_numbers
 
 contents.each do |row|
   id = row[0]
@@ -35,7 +50,20 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  form_letter = erb_template.result(binding)
+  form_letter = letter_template.result(binding)
 
   save_thank_you_letters(id, form_letter)
+end
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  
+  phone = clean_phone(row[:homephone])
+  
+  # form_numbers = number_template.result(binding)
+  
+  # save_phone_numbers(id, form_numbers)
+  
+  ### How do we make this one file created outside the loop?
 end
