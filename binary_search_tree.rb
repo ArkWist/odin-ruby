e01557955b459ec76997c0ca2495f937476efbe4
Daniel A. G. Lilley <arkwist@gmail.com>
2016-04-11 21:25:51 +0900

    Initial commit, forgot to sync further worked on file from other computer

---
+++ binary_search_tree.rb
@@ -0,0 +1,32 @@
+#BST Structure
+#Crawler function
+
+
+class Node
+  # value: getter, setter
+  # parent: getter, setter
+  # child1: getter, setter
+  # child2: getter, setter
+end
+
+def build_tree(data) # << [1,7,4,23,8,9,4,3,5,7,9,67,6345,324]
+                     # Array i ssorted
+end
+
+# Then make able to handle unsorted data
+# Can't pre-sort, must build and adjust tree in order
+
+
+# Then
+def breadth_first_search
+  # Returns node with given value
+  # Use queue array to keep track of child nodes
+end
+
+def depth_first_search
+  # Use stack array
+end
+
+def dfs_rec
+  # depth first search that is recursive
+end
