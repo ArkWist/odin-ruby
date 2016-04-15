73217b1c105f4bc9932f5379d986add583aa3a6c
ArkWist <arkwist@gmail.com>
2016-04-15 12:52:27 +0900

    Work on RBTree class

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,6 +1,5 @@
 class Node
   attr_accessor :value, :parent, :child1, :child2
-
   def initialize(value, parent, color=true) #true = black
     @value = value
     @parent = parent
@@ -10,14 +9,17 @@ class Node
   end
 end
 
-class BST
-  attr_accessor :root
-
-  def initialize(data)
-    @root = build_tree(data)
+class RBTree
+  attr_accessor :head
+  def initialize(head)
+    @head = head
   end
-end
 
+  def add_node(value)
+    #
+  end
+
+end
 
 
 
@@ -29,19 +31,22 @@ def build_tree(data, parent=nil)
   return node
 end
 
-def build_rb_tree(data, parent=nil) #[3,4,23,8,6345,7,9,1,324,7,4,5,67,9]
+
+def build_rb_tree(data) #[3,4,23,8,6345,7,9,1,324,7,4,5,67,9]
+  head = Node.new(data.shift, nil, color)
+  tree = RBTree.new(head)
+
+  data.each do |value|
+    tree.add_node(value)
+  end
+  
   # Add nodes in order.
   # If smaller than head, left child
   # If larger than head, right child
   # If tree unbalanced, balance tree somehow
   #node = Node.new(data[0], parent)
   #node.child1 = build_rb_tree(data[1])
-  head = Node.new(data.shift)
-  data.each do |value|
-    add_node(head, value)
-  end
 end
-
 
 
 
@@ -57,3 +62,11 @@ end
 def dfs_rec
   # depth first search that is recursive
 end
+
+
+sorted_data = [1, 3, 4, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345]
+tree = build_tree(sorted_data)
+
+unsorted_data = [3, 4, 23, 8, 6345, 7, 9, 1, 324, 7, 4, 5, 67, 9]
+rbtree = build_rb_tree(unsorted_data)
+
