7f12808ea10a6083798e2bd4bf95dc7af7b327b7
ArkWist <arkwist@gmail.com>
2016-04-20 15:40:42 +0900

    Write RBT balancing cases

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,19 +1,34 @@
 class Node
   attr_accessor :value, :parent, :child1, :child2
-  def initialize(value, parent=nil, color=true) #true = black
+
+  def initialize(value, parent=nil, black=true)
     @value = value
     @parent = parent
-    @color = color
+    @black = black
     @child1 = nil
     @child2 = nil
   end
+
   def has_child?
     return !child1.nil? || !child.nil?
+  end
+
+  def black?
+    return @black
+  end
+
+  def set_black
+    @black = true
+  end
+
+  def set_red
+    @black = false
   end
 end
 
 class RBT
   attr_accessor :head
+
   def initialize(head)
     @head = head
   end
@@ -31,117 +46,81 @@ class RBT
       end
     end
     node.parent = parent
-    node.color = false
+    node.set_red
     value < parent.value ? parent.child1 = node : parent.child2 = node
-
     rebalance(node)
-
-    end
-
+    @head = find_head(node)
   end
 
-# Red-Black Tree Visualization:
-# https://www.cs.usfca.edu/~galles/visualization/RedBlack.html
-# https://en.wikipedia.org/wiki/Tree_rotation
-
-# RECOLOR when red node's parent's sibling is red
-# RESTRUCTURE when red node's parent's sibling is black or null
-#   RIGHT when added to left
-#   RIGHT-LEFT when added to right on left side
-#   LEFT when added to right
-#   LEFT-RIGHT when added to left on right side
-# Right: swap parent with left child
-# Left: swap parent with right child
-#   is parent right child
-#   current left child becomes parent
-#   parent right child becomes current left child
-# If N's parent is black, do nothing
-# If N's parent is red
-#   If N's parent's sibling is black
-#     If N is left child >> rotate right
-#     If N is right child >> N becomes grandparent
-#     Opposites for opposite side of G I guess.
-
-
-
-
-=begin
-Insert X
-while X != root && P.color == red
-  if P == P.P.left
-    Y = P.P.right  #uncle
-    if Y.color = red
-      then case1
-    elsif X == P.right
-      then case2
-      case3
-    else (case1 with L and R swapped?)
-root.color = black
-
-Page 24:
-http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-introduction-to-algorithms-sma-5503-fall-2005/video-lectures/lecture-10-red-black-trees-rotations-insertions-deletions/lec10.pdf
-
-=end
-
-
-
-=begin
-http://pages.cs.wisc.edu/~paton/readings/Red-Black-Trees/
-
-if P == red
-  if U == black
-    if P == G.left
-      if N == P.left
-        >> P.right = G
-        >> P.parent = G.parent
-        >> G.left = P.right
-        >> G.parent = P
-        >> P.color = black
-        >> G.color = red
-      if N == P.right
-        >> N.left = P
-        >> N.right = G
-        >> N.parent = G.parent
-        >> P.right = null
-        >> P.parent = N
-        >> G.left = null
-        >> G.parent = N
-        >> N.color = black
-        >> G.color = red
-    if P == G.right
-      if N = P.left
-        >> N.right = P
-        >> N.left = G
-        >> N.parent = G.parent
-        >> P.left = null
-        >> P.parent = N
-        >> G.right = null
-        >> G.parent = N
-        >> N.color = black
-        >> G.color = red
-      if N = P.right
-        >> P.left = G
-        >> P.parent = G.parent
-        >> G.right = P.left
-        >> G.parent = P
-        >> P.color = black
-        >> G.color = red
-  if U == red
-
-=end
-  def rebalance(node)
-    # Needs to determine R, L, RL, LR, and recolor
-    while node.color == false && !node.parent.nil? && node.parent.color == false
-      unless node.parent.parent.nil?
-        node.parent.color = true
-        node = parent
-      end
-    end
-
+  def rebalance(N)
+    case_1(N)
   end
 
+  def case_1(N)
+    P = N.parent
+    if P == null then N.set_black else case_2(N, P) end
+  end
+
+  def case_2(N, P)
+    if !P.black? then case_3(N, P) end
+  end
+
+  def case_3(N, P)
+    G = P.parent
+    if P == G.left then U = G.right else U = G.left end
+    if U == null || U.black?
+      case_4(N, P, G)
+    else
+      P.set_black
+      U.set_black
+      G.set_red
+      case_1(G)
+    end
+  end
+
+  def case_4(N, P, G)
+    if N == P.right && P == G.left
+      rotate_left(N, P, G)
+      P = N
+      N = N.left
+    elsif N == P.left && P == G.left
+      rotate_right(N, P, G)
+      P = N
+      N = N.right
+    end
+    case_5(N, P, G)
+  end
+
+  def case_5(N, P, G)
+    P.set_black
+    G.set_red
+    if N == P.left
+      rotate_right(N, P, G)
+    else
+      rotate_left(N, P, G)
+    end
+  end
+
+  def rotate_left(N, P, G)
+    P.right = N.left
+    P.parent = N
+    N.left = P
+    N.parent = G
+    G.left = N
+  end
+
+  def rotate_right(N, P, G)
+    P.left = N.right
+    P.parent = N
+    N.right = P
+    N.parent = G
+    G.right = N    
+  end
+
+  def find_head(N)
+    if N.parent != null then find_head(N.parent) else return N end
+  end
 end
-
 
 
 def build_tree(data, parent=nil)
@@ -156,20 +135,10 @@ end
 def build_rb_tree(data) #[3,4,23,8,6345,7,9,1,324,7,4,5,67,9]
   head = Node.new(data.shift)
   tree = RBT.new(head)
-
   data.each do |value|
     tree.add_node(value)
-    tree.rebalance
   end
-  
-  # Add nodes in order.
-  # If smaller than head, left child
-  # If larger than head, right child
-  # If tree unbalanced, balance tree somehow
-  #node = Node.new(data[0], parent)
-  #node.child1 = build_rb_tree(data[1])
 end
-
 
 
 def breadth_first_search
@@ -191,4 +160,8 @@ tree = build_tree(sorted_data)
 
 unsorted_data = [3, 4, 23, 8, 6345, 7, 9, 1, 324, 7, 4, 5, 67, 9]
 rbtree = build_rb_tree(unsorted_data)
+
+
+
+
 
