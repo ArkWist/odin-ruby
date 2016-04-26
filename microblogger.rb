2737fb879026791e111c1cb2642e11998a2573c3
Daniel A. G. Lilley <arkwist@gmail.com>
2016-04-26 10:53:06 +0900

    Add new MicroBlogger... doesn't work

--- microblogger.rb
+++ microblogger.rb
@@ -7,4 +7,11 @@ class MicroBlogger
     puts "Initializing MicroBlogger"
     @client = JumpstartAuth.twitter
   end
+  
+  def tweet(message)
+    @client.update(message)
+  end
 end
+
+blogger = MicroBlogger.new
+blogger.tweet("MicroBlogger Initialized")
