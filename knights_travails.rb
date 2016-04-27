05d7cb3f75d86dd2c5898651b33759aa9ba22efa
Daniel A. G. Lilley <arkwist@gmail.com>
2016-04-27 19:51:17 +0900

    Initial commit

---
+++ knights_travails.rb
@@ -0,0 +1,19 @@
+
+=begin
+0 1 2 3 4 5 6 7 8 - X [0]
+1
+2
+3
+4
+5
+6
+7
+8 - Y [1]
+=end
+
+# Make board and knight
+# Knight can move forward 2 and side 1, any direction, if doesn't go off edge
+# Possible moves are children in a tree
+### Which algorithm is good for this? Should allow infinite series
+# Find shortest path from starting square to target square
+# Output that path in a list
