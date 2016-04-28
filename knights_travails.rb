516cd429c1073899e69f34b7b02004870b46cc72
ArkWist <arkwist@gmail.com>
2016-04-28 20:05:47 +0900

    Add possible positions

--- knights_travails.rb
+++ knights_travails.rb
@@ -81,9 +81,19 @@ class Move
     @pos = pos
   end
   def get_next
+    x, y = @pos.x, @pos.y
     positions = []
-    positions.push(Position.new())expand_next
-    
+    positions.push(Position.new(x - 2, y - 1))
+    positions.push(Position.new(x - 2, y + 1))
+    positions.push(Position.new(x - 1, y - 2))
+    positions.push(Position.new(x - 1, y + 2))
+    positions.push(Position.new(x + 1, y - 2))
+    positions.push(Position.new(x + 1, y + 2))
+    positions.push(Position.new(x + 2, y - 1))
+    positions.push(Position.new(x + 2, y + 1))
+    positions = clear_invalid(positions)
+     
+
     
     moves = get_moves
     @next = push(Move.new(self, Position.new(@pos.x - 2, @pos.y - 1))
@@ -93,111 +103,3 @@ class Move
     a, b, moves = @pos.x, @pos.y, []
     moves
 #
-
-for i = -2 until i = 3
-  x = -2
-  
-  next if i == 0
-  
-  i = 2
-  def expand(i = 2)
-    return if i == -3
-    next if i == 0
-    Position.new()
-    
-
-
-    a = -2
-    b = -2
-
-x-2, y-1
-x-2, y+1
-x-1, y-2
-x-1, y+2
-x+1, y-2
-x+1, y+2
-x+2, y-1
-x+2, y+1
-
--2, -1
-+0, +2
-+1, -3
-+0, +4
-
-
-
-
-1, 2
-
-  def get_next
-    a, b, moves = 1, 2, []
-    moves += expand(a, b)
-    moves += expand(a * -1, b)
-    moves += expand(b, a)
-    moves += expand(b * -1, a)
-take a, return with b and -b
-take -a, return with b and -b
-take b, return with a and -a
-take -b, return with a and -a
-
-  def expand(a, b)
-    moves = [Position.new(a, b), Position.new(a, b * -1)]
-  end
-
-[]
-
--2, -1, 1, 2
-
-
-    
-=begin
-0 1 2 3 4 5 6 7 8 - X [0]
-1
-2     -   -  
-3   -       -
-4       O
-5   -       -
-6     -   -
-7
-8 - Y [1]
-=end
-
-[4,4] >> [3,2], [5,2]; [2,3], [6,3]; [2,5], [6,5]; [3,6], [5,6]
-
-
-    
-
-Position
-> Array of next positions
-> If any next are finish, return
-
-
-
-Move
-  prior
-  pos
-  next
-
-
-
-x-2, y-1
-x-2, y+1
-x-1, y-2
-x-1, y+2
-x+1, y-2
-x+1, y+2
-x+2, y-1
-x+2, y+1
-
-
-
-x-2, y-1
-x-2, y+1
-x-1, y-2
-x-1, y+2
-x+1, y-2
-x+1, y+2
-x+2, y-1
-x+2, y+1
-
-
