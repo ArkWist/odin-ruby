ec3368760654aa0208afaa0139d2fbc31b37584f
Daniel A. G. Lilley <arkwist@gmail.com>
2016-04-30 11:08:15 +0900

    Write pathfinding and output

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,126 +1,14 @@
-
-=begin
-0 1 2 3 4 5 6 7 8 - X [0]
-1
-2
-3
-4
-5
-6
-7
-8 - Y [1]
-=end
-
-# Make board and knight
-# Knight can move forward 2 and side 1, any direction, if doesn't go off edge
-# Possible moves are children in a tree
-### Which algorithm is good for this? Should allow infinite series
-### Continue until hits the right position
-# Find shortest path from starting square to target square
-# Output that path in a list
-
-class Board
-  def initialize(x = 1, y = 1)
-    @x_size = x - 1
-    @y_size = y - 1
-  end
-end
-
-class Knight
-  def initialize(position)
-    @pos = pos
-  end
-  def moves
-    
-end
-
 class Position
-  attr_reader :x, :y, :pos
-  def initialize(x = 0, y = 0)
+  attr_reader :x, :y, :xy
+  def initialize(x=0, y=0)
     @x = x
     @y = y
-    @xy = [@x, @y]
-  end
-end
-
-class MovementTree
-  attr_reader :pos, :parent
-  def initialize(pos, parent)
-    @pos = pos
-    @parent = parent
-  end
-end
-
-
-board = Board.new(8, 8)
-start_pos = []
-knight = Knight.new()
-moves = MovementTree.new()
-
-
-Creating a knight for each possible position?
-
-###############
-
-start = Position.new(0,0)
-finish = Position.new(3,3)
-
-def knight_moves(start, target)
-  knight = Knight.new(start)
-  moves = [start]
-  until moves.empty?
-    current = moves.shift
-    return current if current.xy == finish.xy
-    moves.push current
-###
-
-
-class Move
-  def initialize(prior=nil, pos)
-    @prior = prior
-    @pos = pos
-  end
-  def get_next
-    x, y = @pos.x, @pos.y
-    positions = []
-    positions.push(Position.new(x - 2, y - 1))
-    positions.push(Position.new(x - 2, y + 1))
-    positions.push(Position.new(x - 1, y - 2))
-    positions.push(Position.new(x - 1, y + 2))
-    positions.push(Position.new(x + 1, y - 2))
-    positions.push(Position.new(x + 1, y + 2))
-    positions.push(Position.new(x + 2, y - 1))
-    positions.push(Position.new(x + 2, y + 1))
-    positions = clear_invalid(positions)
-     
-  end
-  def clear_invalid(positions)
-  
-  end
-end
-    
-    moves = get_moves
-    @next = push(Move.new(self, Position.new(@pos.x - 2, @pos.y - 1))
-#
-
-  def expand_next
-    a, b, moves = @pos.x, @pos.y, []
-    moves
-#
-
-
-##############################################################
-
-
-class Position
-  attr_reader :xy
-  def initialize(x=0, y=0)
     @xy = [x, y]
   end
 end
 
-
 class Move
+  attr_reader :prior, :pos
   def initialize(prior=nil, pos)
     @prior = prior
     @pos = pos
@@ -157,9 +45,63 @@ class Move
     end
     valid
   end
-#
+end
 
 
-#
+
+def knight_moves(knight, target)
+  last_move = get_last_move(knight, target)
+  path = get_path(last_move)
+  puts path_output(path)
+end
+  
+def get_last_move(knight, target)
+  moves, last_move = [Move.new(knight)], nil
+  until last_move
+    next_moves = []
+    moves.each{ |move| next_moves << move.next_moves }
+    next_moves.flatten!
+    next_moves.each do |move|
+      last_move ||= move if move.pos.xy == target.xy
+    end
+    moves = next_moves
+  end
+  last_move
+end
+
+def get_path(last_move)
+  prior = last_move.prior
+  moves = [last_move]
+  until prior == nil
+    moves << prior
+    prior = moves.last.prior
+  end
+  moves.reverse!
+end
+
+def path_output(path)
+  output = "You made it in #{((path.length)-1).to_s} moves! Here's your path:"
+  path.each do |move|
+    output += "\n #{move.pos.xy.to_s}"
+  end
+  output
+end
+
+
+
+knight = Position.new(0, 0)
+target = Position.new(1, 2)
+knight_moves(knight, target)  # 1: [0,0] [1,2]
+
 knight = Position.new(0, 0)
 target = Position.new(3, 3)
+knight_moves(knight, target)  # 2: [0,0] [1,2] [3,3]
+
+knight = Position.new(3, 3)
+target = Position.new(0, 0)
+knight_moves(knight, target)  # 2: [3,3] [1,2] [0,0]
+
+knight = Position.new(3, 3)
+target = Position.new(4, 3)
+knight_moves(knight, target)  # 3: [3,3] [4,5] [2,4] [4,3]
+
