8f669a093099403722de8dc68d1317a10423f1e0
Daniel A. G. Lilley <arkwist@gmail.com>
2016-05-04 19:01:40 +0900

    Plan tests to make

--- project_rspec_testing/enumerable/spec/enumerable_spec.rb
+++ project_rspec_testing/enumerable/spec/enumerable_spec.rb
@@ -1,6 +1,18 @@
 # spec/enumerable_spec.rb
 require "enumerable.rb"
 
+# .myeach
+# .my_each_with_index
+# .my_select
+# .my_all?
+# my_any?
+# my_none?
+# my_count
+# my_map
+# my_inject << multiple_els
+# my_map << with proc
+# my_map << with proc, block, or both, but do block only if both given
+
 describe "enumerable" do
   context "" do
     it "" do
@@ -10,3 +22,6 @@ describe "enumerable" do
   
   
 end
+
+
+
