394298ef305a2f1f61c8dce0b1463630f045206c
Daniel A. G. Lilley <arkwist@gmail.com>
2016-05-04 15:54:51 +0900

    Finish refactoring methods

--- project_rspec_testing/caesar_cipher/lib/cipher.rb
+++ project_rspec_testing/caesar_cipher/lib/cipher.rb
@@ -1,12 +1,3 @@
-def caesar_cipher(string, shift_factor)
-  code = string.each_char.map do |letter|
-    letter = shift_if_range(letter, shift_factor, "a".ord, "z".ord)
-    letter = shift_if_range(letter, shift_factor, "A".ord, "Z".ord)
-  end
-  code.join
-end
-
-
 
 def caesar_cipher(text, shift)
   code = text.each_char.map do |letter|
@@ -19,41 +10,23 @@ def encode(letter, shift)
   if letter.between?("a", "z")
     letter = shift_in_range(letter, shift, "a", "z")
   elsif letter.between?("A", "Z")
-    letter = shift_in_range(letter, shift, "A", "Z")
-  else
-    letter
+    letter = bounded_shift(letter, shift, "A", "Z")
   end
-end
-
-def shift_in_range(letter, shift, range_a, range_z)
-# Implement shifting letter only within given range
-
-
-
-
-
-def shift_if_range(letter, shift_factor, range_start, range_end)
-
-  letter_number = letter.ord
-  
-  if letter_number >= range_start && letter_number <= range_end then
-
-    # Sanitize shift_factor so don't need while loop
-    shift_factor = shift_factor.to_i
-    shift_factor = shift_factor % (range_end - range_start)
-    
-    letter_number += shift_factor
-
-    if letter_number > range_end then
-      shift_factor = letter_number - range_end
-      letter_number = range_start + shift_factor
-    end
-
-    letter = letter_number.chr
-  end
-
   letter
 end
+
+def bounded_shift(letter, shift, range_a, range_z)
+  shift = shift % (range_z.ord - range_a.ord)
+  # ^ Handle shifts greater than the given range.
+  letter = (letter.ord + shift).chr
+  if letter.ord > range_z.ord
+    shift = letter.ord - range_z.ord
+    letter = (range_a.ord + shift).chr
+  end
+  letter
+end
+
+
 
 # Ask for input only when run independently (not through Rspec).
 if __FILE__ == $0
