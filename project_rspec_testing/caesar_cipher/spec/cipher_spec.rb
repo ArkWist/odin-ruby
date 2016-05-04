8d8f53c2ba2cc8ca0adb43873cdacb68005a43b8
Daniel A. G. Lilley <arkwist@gmail.com>
2016-05-04 18:13:03 +0900

    Pass all tests!

--- project_rspec_testing/caesar_cipher/spec/cipher_spec.rb
+++ project_rspec_testing/caesar_cipher/spec/cipher_spec.rb
@@ -1,18 +1,9 @@
 # spec/cipher_spec.rb
-
 require "cipher.rb"
-
-# shifts text
-# wraps from z to a
-# keeps case
-# handles shift of 0
-# handles shift over 26
-# handles negative shift
-# handles shift with punctuation
 
 describe "caesar_cipher" do
   context "given text and shift factor" do
-    it "returns shifted text" do
+    it "shifts a to z" do
       expect(caesar_cipher("hello", 2)).to eq("jgnnq")
     end
   end
@@ -36,13 +27,32 @@ describe "caesar_cipher" do
   end
   
   context "given shift over 26" do
-    it "wraps a to z multiple times" do
+    it "wraps a to z" do
       expect(caesar_cipher("ForGG", 27)).to eq("GpsHH")
     end
   end
-      
-      
   
+  context "given shift multiply over 26" do
+    it "wraps a to z multiple times" do
+      expect(caesar_cipher("ForGG", 53)).to eq("GpsHH")
+    end
+  end
+
+  context "given negative shift" do
+    it "shifts z to a" do
+      expect(caesar_cipher("rabbit", -1)).to eq("qzaahs")
+    end
+  end
   
+  context "given negative shift under -26" do
+    it "wraps z to a" do
+      expect(caesar_cipher("rabbit", -27)).to eq("qzaahs")
+    end
+  end
   
+  context "given non-alphabetical letters" do
+    it "maintains non-alphabetical letters" do
+      expect(caesar_cipher("This... is a test!", 1)).to eq("Uijt... jt b uftu!")
+    end
+  end
 end
