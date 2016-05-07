58f7e52461b34977ca11ad32641294643d7217c0
Daniel A. G. Lilley <arkwist@gmail.com>
2016-05-07 12:53:16 +0900

    Write Rspec tests up to .my_all?

--- project_rspec_testing/enumerable/spec/enumerable_spec.rb
+++ project_rspec_testing/enumerable/spec/enumerable_spec.rb
@@ -1,9 +1,6 @@
 # spec/enumerable_spec.rb
 require "enumerable.rb"
 
-# .myeach
-# .my_each_with_index
-# .my_select
 # .my_all?
 # my_any?
 # my_none?
@@ -21,18 +18,45 @@ require "enumerable.rb"
 #end
 
 describe Enumerable do
-  subject { [1, 2, 3] }
+  subject { [1, 2, 4] }
 
+  # Don't know how to test this properly
   describe ".my_each" do
-    context "block passed to .my_each" do
-      it "do block on each element" do
-        #expect([1, 2, 3].@dummy_class.my_each{ |i| i + 1 }).to eq([2, 3, 4])
+    context "passed a block" do
+      it "does block on each element" do
+        #expect([1, 2, 3].@dummy_class.my_each{ |i| i + 1 }).to eq([2, 3, 5])
         #expect(data.my_each{ |i| i + 1 }).to eq(data.each{ |i| i + 1 })
-        expect(subject.my_each{ |i| puts i }).to eq("123")
+        #expect(subject.my_each{ |i| print i }).to eq("124")
       end
     end
-    
   end
+    
+  describe ".my_each_with_index" do
+    context "passed a block" do
+      it "does block on each element with index" do
+        #expect(subject.my_each{ |e, i| print "#{i}:#{e} " }).to eq("1:1 2:2 3:4 ")
+      end
+    end
+  end
+  
+  describe ".my_select" do
+    context "passed a boolean block" do
+      it "returns array satisfying the block" do
+        expect(subject.my_select{ |e| e > 1 }).to eq([2, 4])
+      end
+    end
+  end
+  
+  describe ".my_all?" do
+    context "passed a boolean block" do
+      it "returns if all elements satisfy the block" do
+        expect(subject.my_select{ |e| e < 2}).to eq(false)
+        expect(subject.my_select{ |e| e < 5}).to eq(true)
+      end
+    end
+  end
+    
+
 end
 
 
