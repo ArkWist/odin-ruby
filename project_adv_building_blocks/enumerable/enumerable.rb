3ab464f87cdd7f0ba410daf0d17ca88bbd17a0ca
Daniel A. G. Lilley <arkwist@gmail.com>
2016-05-07 23:10:36 +0900

    Correct and finish enumerable.rb

--- project_adv_building_blocks/enumerable/enumerable.rb
+++ project_adv_building_blocks/enumerable/enumerable.rb
@@ -1,96 +1,80 @@
 module Enumerable
 
-  def my_each   # ; do the given block for everything in the array 
+  def my_each
     for element in self
       yield(element)
     end
   end
   
-  def my_each_with_index  # Can I make this piggyback off my_each?
+  def my_each_with_index
     index = 0
     for element in self
-      yield(element,index)
+      yield(element, index)
       index += index
     end
   end
   
   def my_select
     selected = []
-    my_each { |element| selected << element if yield(element) }
+    my_each{ |element| selected << element if yield(element) }
     selected
   end
   
   def my_all?
-    my_each { |element| return false unless yield(element) }
+    my_each{ |element| return false unless yield(element) }
     true
   end
   
   def my_any?
-    my_each { |element| return true if yield(element) }
+    my_each{ |element| return true if yield(element) }
     false
   end
   
   def my_none?
-    my_each { |element| return false unless !yield(element) }
+    my_each{ |element| return false unless !yield(element) }
     true
   end
   
   def my_count
     count = 0
-    my_each_with_index { |element| count += 1 if yield(element) }
+    my_each_with_index{ |element| count += 1 if yield(element) }
     count
   end
 
-# Original my_map method
-=begin
-  def my_map
-    mapped = []
-    my_each { |element| mapped << yield(element) }
-    mapped
-  end
-=end
-
-# New my_map method with proc
-### NEEDS TESTING AS I'M NOT QUITE FAMILIAR WITH PROCS
-=begin
-  def my_map (procedure)
-                # ; this receives a proc as an argument
-                # ; this proc is used to assign values to each element
-    mapped = []
-    my_each { |element| mapped << procedure.call }
-    mapped
-  end
-=end
-
-# New my_map taking proc, block, or both
-# Execute both if both present
-# Otherwise only execute proc
-### I CAN'T YET FIGURE OUT HOW TO DO THIS AND SO MUCH REVISIT THE PROJECT
-  def my_map (code)
-    # ??
-  end
-
-  def my_inject (injected)
-                # ; accumulates everything with what it's given
-                # ; but this doesn't match what ToP is asking
-                # ; so I think this takes a default value
-                # ; then that can be used in the block passed to inject
-                # ; which could be any operation
-    my_each { |element| injected = yield(injected, element) }
-    injected
-  end
+# Original my_map method that only takes a block.
+  #def my_map
+  #  mapped = []
+  #  my_each{ |element| mapped << yield(element) }
+  #  mapped
+  #end
   
+# Second my_map method that takes only a proc.
+  #def my_map(proc)
+  #  mapped = []
+  #  my_each{ |element| mapped << proc.call(element) }
+  #  mapped
+  #end
+  
+# New my_map method that takes a proc, a block, or both.
+# However, only executes the block when supplied with a proc.
+#   In this case, both the block and proc are executed.
+# This is meant to do nothing if only a block is supplied.
+  def my_map(proc = nil)
+    mapped = []
+    if proc && !block_given?
+      my_each{ |element| mapped << proc.call(element) }
+    elsif proc && block_given?
+      my_each{ |element| mapped << proc.call(yield(element)) }
+    end
+    mapped  
+  end
+
+  def my_inject(memo = self[0])
+    my_each{ |element| memo = yield(memo, element) }
+    memo
+  end
 end
 
-def multiply_els (array)
-  array.my_inject(1) { |result, element| result * element}
+def multiply_els(array)
+  array.my_inject(1) { |result, element| result * element }
 end
-
-
-@array = [1, 3, 2, 6, 2, 5, 4, 5]
-
-# Test my_each ; can't test because repl.it doesn't like modules
-# @array.my_each { |element| print element}
-
-# print multiply_els(array)
-
