386278030a1af5b58761ee53e9f26a98d1881ae8
Daniel A. G. Lilley <arkwist@gmail.com>
2016-05-09 14:31:13 +0900

    Write test for .new

--- project_rspec_testing/connect_four/lib/connect_four.rb
+++ project_rspec_testing/connect_four/lib/connect_four.rb
@@ -1,1 +1,22 @@
-# Connect Four
+# ConnectFour
+
+class ConnectFour
+  PLAYERS = [:X, :O]
+  attr_reader :board, player
+
+  def initialize
+    width, height = 7, 6
+    @board = Board.new(width, height)
+    prepare_game
+  end
+  
+  def prepare_game
+    @board.wipe
+    @player = PLAYERS.first
+  end
+  
+  def play
+  
+  end
+
+end
--- project_rspec_testing/connect_four/spec/connect_four_spec.rb
+++ project_rspec_testing/connect_four/spec/connect_four_spec.rb
@@ -1,5 +1,48 @@
 # spec/connect_four_spec.rb
-require "connect_four.rb"
+require "connect_four"
 
-describe "connect_four.rb" do # Or ConnectFour
-end
+describe "ConnectFour" do
+  let(:c4) { ConnectFour.new }
+  let(:p1) { c4.PLAYERS.first }
+  let(:p2) { c4.PLAYERS.last}
+  
+  describe "#new" do
+    it "creates a new game board" do
+      expect(c4.board).to be_instance_of(Board)
+    end
+    it "sets player to #{ConnectFour::PLAYERS.first}" do
+      expect(c4.player).to eq(p1)
+    end
+  end
+
+end  
+
+=begin
+  describe "#change_player" do
+    it "changes from #{ConnectFour::PLAYERS.first} player to #{ConnectFour::PLAYERS.last} player" do
+      expect(c4.player).to eq(p1)
+      c4.change_player
+      expect(c4.player).to eq(p2)
+    end
+    it "changes from #{ConnectFour::PLAYERS.last} player to #{ConnectFour::PLAYERS.first} player" do
+      c4.change_player
+      expect(c4.player).to eq(p2)
+      c4.change_player
+      expect(c4.player).to eq(p1)
+    end
+  end
+=end
+
+=begin
+describe ConnectFour, "#game_set" do
+  context "with 4 discs in a horizontal row" do
+    it "recogizes player's victory" do
+      4.times { c4.drop_disc(p1, 1)  >> 3.times + 1
+                c4.drop_disc(p2, 2) }
+      expect
+      
+      c4.current_player = current_playercurrent_player = connect_four.drop(1) }
+      
+      
+      end
+=end
