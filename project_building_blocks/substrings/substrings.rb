ea5d3b8ffcdbd0a977a66c16659590b199d7b98f
Daniel A. G. Lilley <arkwist@gmail.com>
2016-07-07 16:54:20 +0900

    Refactor

--- project_building_blocks/substrings/substrings.rb
+++ project_building_blocks/substrings/substrings.rb
@@ -1,11 +1,23 @@
+
+
 def substrings (sentence, dictionary)
   all_substrings = Hash.new(0)
   sentence.each do |word|
-    if dictionary.include? word
-      all_substrings[word] = all_substrings[word] + 1
-    end
+    all_substrings[word] = all_substrings[word] + 1 if dictionary.include? word
   end
   all_substrings
+end
+
+def print_substrings(all_substrings)
+  string = "Substrings: {"
+  needs_comma = false
+  all_substrings.each do |key, value|
+    if needs_comma then string += ", " end
+    string += "\"#{key}\"=>#{value}"
+    needs_comma = true
+  end
+  string += "}"
+  puts string
 end
 
 print "Enter word/sentence: "
@@ -15,15 +27,6 @@ print "Enter dictionary words (separated with commas): "
 user_dictionary = gets.chomp.delete(" ").split(",")
 
 all_substrings = substrings(user_sentence, user_dictionary)
-print_string = "Substrings: {"
-needs_comma = false
-all_substrings.each do |key, value|
-  if needs_comma
-    print_string += ", "
-  end
-  needs_comma = true
-  print_string += "\"#{key}\"=>#{value}"
-end
-print_string += "}"
-puts print_string
+
+print_substrings(all_substrings)
 
