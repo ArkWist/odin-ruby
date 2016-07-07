0d435e58f001a1cc0a9f7893c5f1ed01f1f17c5c
Daniel A. G. Lilley <arkwist@gmail.com>
2016-07-07 17:34:25 +0900

    Refactor

--- project_adv_building_blocks/bubble_sort/bubble_sort.rb
+++ project_adv_building_blocks/bubble_sort/bubble_sort.rb
@@ -1,58 +1,42 @@
 def bubble_sort (array)
-  
   sorted = false
-
   while !sorted
     sorted = true
-
     index = 0
-    while index < array.length - 1
-    
+    until index == array.length - 1
       if array[index] > array[index+1]
-        temp = array[index]
-        array[index] = array [index+1]
-        array[index+1] = temp
         sorted = false
+        array[index], array[index + 1] = array[index + 1], array[index]
       end
-      
       index += 1
-      
     end
   end
-  return array
+  array
 end
 
 def bubble_sort_by (array)
-
   sorted = false
-  
   while !sorted
     sorted = true
-    
     index = 0
     while index < array.length - 1
-    
       if yield(array[index], array[index+1]) > 0
-        temp = array[index]
-        array[index] = array [index+1]
-        array[index+1] = temp
         sorted = false
+        array[index], array[index + 1] = array[index + 1], array[index]
       end
-      
       index += 1
-      
     end
   end
-  return array
+  array
 end
 
-# print "Enter array to sort: "
-# unsorted_array = gets.chomp
 
+=begin
 unsorted_array = [1,3,2,5,6,4]
 print bubble_sort(unsorted_array).inspect
 
-sorted_array = bubble_sort_by(["hi","hello","hey"]) do |left,right|
-  left.length - right.length
-end
+sorted_array = bubble_sort_by(["hi","hello","hey"]) { |left,right|
+  left.length - right.length }
 print sorted_array.inspect
+=end
+
