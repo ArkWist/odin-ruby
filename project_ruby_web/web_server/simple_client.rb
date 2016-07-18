4ff5a74f92105c92d027f487ef8119cb69b62b4f
Daniel A. G. Lilley <arkwist@gmail.com>
2016-07-18 12:31:20 +0900

    Fix syntax errors

--- project_ruby_web/web_server/simple_client.rb
+++ project_ruby_web/web_server/simple_client.rb
@@ -6,6 +6,6 @@ port = 2000
 s = TCPSocket.open(hostname, port)
 
 while line = s.gets
-  put line.chop
+  puts line.chop
 end
 s.close
--- project_ruby_web/web_server/simple_server.rb
+++ project_ruby_web/web_server/simple_server.rb
@@ -3,7 +3,7 @@ require "socket"
 server = TCPServer.open(2000)
 loop do
   client = server.accept
-  clients.puts(Time.now.ctime)
+  client.puts(Time.now.ctime)
   client.puts "Closing the connection. Bye!"
   client.close
 end
