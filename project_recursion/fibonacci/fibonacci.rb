69f1c81c8fed3f560ac0a48a2f95a0e24560d53c
ArkWist <arkwist@gmail.com>
2016-08-30 14:43:55 +0900

    Refactor

--- project_recursion/fibonacci/fibonacci.rb
+++ project_recursion/fibonacci/fibonacci.rb
@@ -1,20 +1,20 @@
-def fibs(number)
+def fibs(n)
   fib = []
-  while fib.length < 2 && number - fib.length > 0
+  while fib.length < 2 && n - fib.length > 0
     fib << fib.length
   end
-  (number - 2).times do |i|
-    fib << (fib[i] + fib[i+1])
+  (n - 2).times do |i|
+    fib << (fib[i] + fib[i + 1])
   end
   fib
 end
 
 def fibs_rec(n)
   if n == 1
-    fib = 0
+    [0]
   elsif n == 2
-    fib = [fibs_rec(n - 1), fibs_rec(n - 1) + 1]
+    fibs_rec(n - 1) << fibs_rec(n - 1).at(-1) + 1
   else
-    fib = fibs_rec(n - 1) << (fibs_rec(n - 1).at(-1) + fibs_rec(n - 1).at(-2))
+    fibs_rec(n - 1) << (fibs_rec(n - 1)[-1] + fibs_rec(n - 1)[-2])
   end
 end
